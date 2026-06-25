b1f22db0f2d79529d4f2772f969248d9f92a2b94
Ельцов Дмитрий <eltsob.dmitry@gmail.com>
2026-06-25 11:34:03 +0500

    Search

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,27 @@
 import { addToCart } from '@/entities/cart';
 import { useGetProductsAllInfiniteQuery } from '@/shared/api/products';
+import { useDebouncedValue } from '@/shared/lib/hooks';
 import { useAppDispatch } from '@/shared/lib/store';
+import { Spacing } from '@/shared/ui';
 import { InfiniteScroll, ProductCard } from '@/shared/ui/molecules';
-import { CircularProgress, Typography } from '@mui/material';
-import { IconInfoCircle } from '@tabler/icons-react';
-import { type FC } from 'react';
+import { CircularProgress, TextField, Typography } from '@mui/material';
+import { IconInfoCircle, IconSearch } from '@tabler/icons-react';
+import { useState, type FC } from 'react';
 
 export const HomePage: FC = () => {
-    const { isLoading, isError, products, hasNextPage, fetchNextPage } =
-        useGetProductsAllInfiniteQuery();
     const dispatch = useAppDispatch();
+
+    const [search, setSearch] = useState('');
+    const debouncedValue = useDebouncedValue(search);
+
+    const {
+        isLoading,
+        isError,
+        products,
+        hasNextPage,
+        fetchNextPage,
+        isFetching,
+    } = useGetProductsAllInfiniteQuery(debouncedValue);
 
     if (isLoading) {
         return (
@@ -19,7 +31,7 @@ export const HomePage: FC = () => {
         );
     }
 
-    if (!products.length || isError) {
+    if (isError) {
         return (
             <div className="flex-1 flex flex-col justify-center items-center">
                 <div className="flex flex-col gap-1 justify-center items-center">
@@ -31,20 +43,43 @@ export const HomePage: FC = () => {
     }
 
     return (
-        <div className="flex-1">
-            <div className="grid grid-cols-3 gap-4">
-                {products.map((product) => (
-                    <ProductCard
-                        key={product.id}
-                        product={product}
-                        onAddCart={() => dispatch(addToCart(product))}
-                    />
-                ))}
-            </div>
-            <InfiniteScroll
-                hasNextPage={hasNextPage}
-                fetchNextPage={fetchNextPage}
+        <div className="flex-1 flex flex-col">
+            <TextField
+                value={search}
+                onChange={(val) => setSearch(val.target.value)}
+                slotProps={{
+                    input: {
+                        startAdornment: <IconSearch className="mr-4" />,
+                        endAdornment:
+                            search && isFetching ? (
+                                <CircularProgress size={18} />
+                            ) : null,
+                    },
+                }}
             />
+            <Spacing spacing={4} />
+            {products.length > 0 ? (
+                <>
+                    <div className="grid grid-cols-3 gap-4">
+                        {products.map((product) => (
+                            <ProductCard
+                                key={product.id}
+                                product={product}
+                                onAddCart={() => dispatch(addToCart(product))}
+                            />
+                        ))}
+                    </div>
+                    <InfiniteScroll
+                        hasNextPage={hasNextPage}
+                        fetchNextPage={fetchNextPage}
+                    />
+                </>
+            ) : (
+                <div className="flex flex-1 flex-col gap-1 justify-center items-center">
+                    <IconInfoCircle size={48} />
+                    <Typography variant="h4">Список пуст</Typography>
+                </div>
+            )}
         </div>
     );
 };
--- src/shared/api/products/api.ts
+++ src/shared/api/products/api.ts
@@ -4,17 +4,31 @@ import { useMemo } from 'react';
 
 export const productsApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
-        getProductsAll: builder.infiniteQuery<ProductsResponse, void, number>({
-            infiniteQueryOptions: {
-                initialPageParam: 0,
-                getNextPageParam: (lastPage, allPages, lastPageParam) => {
-                    const nextSkip = lastPageParam + lastPage.limit;
-                    return nextSkip < lastPage.total ? nextSkip : undefined;
+        getProductsAll: builder.infiniteQuery<ProductsResponse, string, number>(
+            {
+                infiniteQueryOptions: {
+                    initialPageParam: 0,
+                    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+                        const nextSkip = lastPageParam + lastPage.limit;
+                        return nextSkip < lastPage.total ? nextSkip : undefined;
+                    },
                 },
+                query: ({ pageParam, queryArg: search }) => {
+                    const params = new URLSearchParams({
+                        limit: '12',
+                        skip: String(pageParam),
+                    });
+
+                    if (search) {
+                        params.set('q', search);
+                        return `products/search?${params.toString()}`;
+                    }
+
+                    return `products?${params.toString()}`;
+                },
+                providesTags: ['Products'],
             },
-            query: ({ pageParam }) => `products?limit=12&skip=${pageParam}`,
-            providesTags: ['Products'],
-        }),
+        ),
         getProductsSingle: builder.query<Product, number>({
             query: (id) => `products/${id}`,
             providesTags: ['Products'],
@@ -24,8 +38,8 @@ export const productsApi = baseApi.injectEndpoints({
 
 export const { useGetProductsSingleQuery } = productsApi;
 
-export const useGetProductsAllInfiniteQuery = () => {
-    const result = productsApi.useGetProductsAllInfiniteQuery();
+export const useGetProductsAllInfiniteQuery = (search: string) => {
+    const result = productsApi.useGetProductsAllInfiniteQuery(search);
     const products = useMemo(
         () => result.data?.pages?.flatMap((page) => page.products) || [],
         [result.data],
