75647ee4a272a4b09c53a4df0381391dda409edc
Ельцов Дмитрий <eltsob.dmitry@gmail.com>
2026-08-30 12:36:08 +0500

    Ленивая загрузка страниц через route.lazy

--- src/application/routes/index.ts
+++ src/application/routes/index.ts
@@ -1,17 +1,34 @@
-import { ProductPage } from '@/pages/product';
-import { HomePage } from '@/pages/home';
 import type { RouteObject } from 'react-router';
 import App from '../App';
-import { FavoritesPage } from '@/pages/favorites';
 
 export const routes: RouteObject[] = [
     {
         path: '/',
         Component: App,
         children: [
-            { index: true, Component: HomePage },
-            { path: 'product/:id', Component: ProductPage },
-            { path: 'favorites', Component: FavoritesPage },
+            {
+                index: true,
+                lazy: async () => {
+                    const { HomePage } = await import(/* webpackChunkName: "home-page" */ '@/pages/home');
+                    return { Component: HomePage };
+                },
+            },
+            {
+                path: 'product/:id',
+                lazy: async () => {
+                    const { ProductPage } = await import(/* webpackChunkName: "product-page" */ '@/pages/product');
+                    return { Component: ProductPage };
+                },
+            },
+            {
+                path: 'favorites',
+                lazy: async () => {
+                    const { FavoritesPage } = await import(
+                        /* webpackChunkName: "favorites-page" */ '@/pages/favorites'
+                    );
+                    return { Component: FavoritesPage };
+                },
+            },
         ],
     },
 ];
